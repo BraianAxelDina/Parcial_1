067e4894fa170a3be360318fef706c0810c36ed3
BraianAxelDina <braian.dina@gmail.com>
2024-10-26 15:10:55 -0300

    creacion de archivo .py con ejecicio 1 de paso #2

---
+++ practico01/Tipos_datos/2_1-NumYOper.py
@@ -0,0 +1,19 @@
+#Ejercicio 2.1
+
+a = 7
+b = 3
+
+#Se concatena la presentacion de la informacion con la operacion solicitada
+#como metodo de aproximacion a las consignas
+
+#Suma
+print ("El resultado de la sumatoria de ", a , "y " , b ," es: ", a+b , "\n")
+#Resta
+print ("El resultado de la resta entre ", a , "y " , b ," es: ", a-b , "\n")
+#Multiplicacion
+print ("El resultado de la multiplicacion de ", a , "y " , b ," es: ", a*b , "\n")
+#Division
+print ("El resultado de la division entre ", a , "y " , b ," es: ", a/b , "\n")
+#Modulo
+print ("El resto de la division entre ", a , "y " , b ," es: ", a%b , "\n")
+
